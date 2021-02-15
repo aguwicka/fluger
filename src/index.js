be839606bfa1e3f2664ff75c96f12478843e4d37
pashasema <dosurgeest@gmail.com>
2021-02-15 15:00:07 +0300

    mobile adaptive for footer

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import html from "./template/pages/index.pug"
 import style from "./styles/main.scss";
 // Import scripts
 import 'magnific-popup/dist/jquery.magnific-popup.min.js';
-import 'inputmask/dist/jquery.inputmask.min';
+// import 'inputmask/dist/jquery.inputmask.min';
 import script from "./scripts/main.js";
 
 console.log('all files loaded');
