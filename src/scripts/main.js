5cd6eef42dd4b2e65f3341501840f03d307d83b8
aguwicka <aguwicka@yandex.ru>
2021-02-25 10:21:12 +0300

    header + cataloglvl3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -127,3 +127,10 @@ $(function () {
     slidersOnMainP();
 
   });
+
+//toggle mobile menu
+let toggleButton = document.querySelector('.toggle-menu');
+let navBar = document.querySelector('.nav-bar');
+toggleButton.addEventListener('click', function () {
+    navBar.classList.toggle('toggle');
+});
